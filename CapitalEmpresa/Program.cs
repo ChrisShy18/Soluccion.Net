1ad444a3c7bd3790deca5e67ff6675bcfd482fce
JADPA-11 <JADPA-11@JADPA-11>
2022-08-04 10:58:22 -0600

    Agregar archivos de proyecto.

---
+++ CapitalEmpresa/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("CAPITAL DE EMPRESA");
+Console.WriteLine("");
+
+double monto1, monto2, monto3;
+
+Console.WriteLine("INGRESE EL MONTO QUE APORTO EL PRIMER SOCIO: C$ ");
+monto1 = double.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL MONTO QUE APORTO EL SEGUNDO SOCIO: C$");
+monto2 = double.Parse(Console.ReadLine());
+Console.WriteLine("INGRESE EL MONTO QUE APORTO EL TERCER SOCIO: C$ ");
+monto3 = double.Parse(Console.ReadLine());
+
+double capital;
+capital= monto1 + monto2 + monto3;
+
+double PSocio1, PSocio2, PSocio3;
+PSocio1 = (monto1 * 100) / capital;
+PSocio2 = (monto2 * 100) / capital;
+PSocio3 = (monto3 * 100) / capital;
+
+Console.WriteLine("EL PROMEDIO DEL PRIMER SOCIO ES: " + PSocio1.ToString("0.00"));
+Console.WriteLine("EL PROMEDIO DEL SEGUNDO SOCIO ES: " + PSocio2.ToString("0.00"));
+Console.WriteLine("EL PROMEDIO DEL TERCER SOCIO ES: " + PSocio3.ToString("0.00"));
---
+++ ConversorMedidas/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(" **cONVERSION DE MEDIDAS **");
+Console.WriteLine("");
+
+double metros;
+
+Console.WriteLine("INGRESE LA MEDIA DE LA TELA EN METROS");
+metros = int.Parse(Console.ReadLine());
+
+double centimetro, yarda, pulgada, pies;
+centimetro = metros * 100.0;
+pulgada = centimetro / 2.54;
+pies = pulgada / 12.0;
+yarda = pies / 3.0 ;
+
+Console.WriteLine("*******************");
+Console.WriteLine("LA MEDIDA EN CENTIMETROS ES:"  +centimetro.ToString("0.00"));
+Console.WriteLine("LA MEDIDA EN PULGADAS ES : " + pulgada.ToString("0.00"));
+Console.WriteLine("LA MEDIDA EN PIES ES: " +pies.ToString("0.00"));
+Console.WriteLine("LA MEDIDA EN YARDAS ES: " +yarda.ToString("0.00"));
+Console.WriteLine("*******************");
+
+
+
+
+
